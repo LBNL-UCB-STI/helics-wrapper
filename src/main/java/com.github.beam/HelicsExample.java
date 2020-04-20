9ee13d3c0e195f0af540e7fbe01a118cdba07953
SHaaD94 <eugenzuy@gmail.com>
2020-04-20 20:15:06 +0700

    Changes

--- src/main/java/com.github.beam/HelicsExample.java
+++ src/main/java/com.github.beam/HelicsExample.java
@@ -1,5 +1,6 @@
 package com.github.beam;
 
+import com.java.helics.SWIGTYPE_p_void;
 import com.java.helics.helics;
 
 public class HelicsExample {
@@ -7,5 +8,10 @@ public class HelicsExample {
         HelicsLoader.load();
 
         System.out.println("HELICS Version: " + helics.helicsGetVersion());
+        SWIGTYPE_p_void broker = helics.helicsCreateBroker("zmq", "", "-f 2 --name=BeamBrokerTemp");
+        System.out.println(helics.helicsBrokerIsConnected(broker));
+        helics.helicsBrokerDestroy(broker);
+        System.out.println(helics.helicsBrokerIsConnected(broker));
+        helics.helicsCloseLibrary();
     }
 }
