fe73e7beca0cbe2a19dd6a6bce05cf92245d5d3d
Nikolay Ilyin <irishwithaxe@gmail.com>
2022-10-19 14:34:12 +0300

    added libhezmq5 to mac archive

--- src/main/shell/pack_archives.sh
+++ src/main/shell/pack_archives.sh
@@ -19,6 +19,7 @@ os="mac" && rm -rf $os && mkdir $os
 cp ../downloads/regular/*macOS*/java/libhelicsJava.dylib $os/libhelicsJava.dylib
 cp ../downloads/regular/*macOS*/lib/libhelics.3.dylib $os/libhelics.3.dylib
 cp ../downloads/regular/*macOS*/lib/libzmq.dylib $os/libzmq.dylib
+cp ../downloads/regular/*macOS*/lib/libzmq.5.dylib $os/libzmq.5.dylib
 cp ../downloads/shared/*macOS*/lib/libhelics.dylib $os/libhelicsSharedLib.dylib
 cd $os && zip ../$os.zip * && cd ..
 cp $os.zip ../../resources/helics-binaries/$os.zip
